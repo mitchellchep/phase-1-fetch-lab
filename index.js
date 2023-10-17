2a8a1a325f9dfaa726fa2ea29938b2ce9c430e24
mitchellkoech <mitchikoechi3.7@gmail.com>
2023-10-17 09:40:34 +0300

    Completed assignment
    Signed-off-by: mitchellkoech <mitchikoechi3.7@gmail.com>

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books") // Fetch the list of books from the API
+    .then((resp) => resp.json()) // Extract the JSON data from the response
+    .then((json) => renderBooks(json)); // Call the renderBooks() function with the JSON data
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
+  const main = document.querySelector('main'); // Get the <main> element from the DOM
   books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+    const h2 = document.createElement('h2'); // Create an <h2> element for each book
+    h2.innerHTML = book.name; // Set the book title as the content of the <h2> element
+    main.appendChild(h2); // Append the <h2> element to the <main> element in the DOM
   });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+  // When the DOM is fully loaded, execute the fetchBooks() function
   fetchBooks();
 });
